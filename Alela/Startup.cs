ecd95de735ed56af26e2a428e4f1059c4c247f63
Goran <Goran@AXANAR>
2017-09-23 15:13:20 +0200

    Add project files.

---
+++ Alela/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Alela.Startup))]
+namespace Alela
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
